0b50923d1a6c18a918c684be598689ded142eb71
hussalen <s26136@pjwstk.edu.pl>
2025-01-14 22:30:26 +0100

    fix: resolve non-test compiler errors

--- project/Program.cs
+++ project/Program.cs
@@ -1,7 +1,1 @@
 ﻿using assignment_3;
-
-Student student = new Student(ClassLevel.Freshman);
-var validGpa = new Student(ClassLevel.Freshman);
-var gr = new Grade(4);
-validGpa.AddGrade(gr);
-validGpa.AddGrade(gr);
